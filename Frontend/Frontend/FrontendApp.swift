dd417eb508fe0657f488405bd52038cde4cb5d7f
aconopio <aconopio@uoguelph.ca>
2026-01-17 14:01:39 -0500

    Init swift app

---
+++ Frontend/Frontend/FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontendApp.swift
+//  Frontend
+//
+//  Created by Antonio Conopio on 2026-01-17.
+//
+
+import SwiftUI
+
+@main
+struct FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
