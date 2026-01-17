cb739e7f15402618e750e1e14e125f6c8690b388
Dion Calim <Dioncalim@gmail.com>
2026-01-17 14:01:54 -0500

    backend

---
+++ Backend/src/main/java/com/HomeHero/demo/HomeHeroApplication.java
@@ -0,0 +1,13 @@
+package com.HomeHero.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeHeroApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/HomeHero/demo/HomeHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HomeHero.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
