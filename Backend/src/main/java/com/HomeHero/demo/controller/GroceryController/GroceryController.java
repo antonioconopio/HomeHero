a523e8a51036c03f34e297e500a6980df39ce220
Dion Calim <Dioncalim@gmail.com>
2026-01-18 01:59:41 -0500

    CRUD for grocery feature

--- Backend/src/main/java/com/HomeHero/demo/controller/GroceryController/GroceryController.java
+++ Backend/src/main/java/com/HomeHero/demo/controller/GroceryController/GroceryController.java
@@ -3,10 +3,12 @@ package com.HomeHero.demo.controller.GroceryController;
 import com.HomeHero.demo.model.Grocery;
 import com.HomeHero.demo.service.grocery.GroceryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,9 +23,26 @@ public class GroceryController {
     }
 
     @RequestMapping(value = "/getGrocery", produces = "application/json", method = RequestMethod.GET)
-    public Grocery getGrocery() {
+    public List<Grocery> getGrocery(String household_id) {
         //String userId = (String) authentication.getPrincipal();
 
-        return new Grocery();
+        return groceryService.getGroceries("5ca95d0d-e4f9-4957-9092-bb8f76643932");
+    }
+
+    @RequestMapping(value = "/insertGrocery", produces = "application/json", consumes = "application/json", method = RequestMethod.POST)
+    public Grocery insertGrocery(@RequestBody Grocery grocery) {
+        //String userId = (String) authentication.getPrincipal();
+
+        return groceryService.insertGrocery(grocery);
+    }
+
+    @RequestMapping(value = "/deleteGrocery", produces = "application/json", consumes = "application/json", method = RequestMethod.DELETE)
+    public Grocery deleteGrocery(@RequestBody Grocery grocery) {
+        return groceryService.deleteGrocery(grocery);
+    }
+
+    @RequestMapping(value = "/updateGrocery", produces = "application/json", consumes = "application/json", method = RequestMethod.PUT)
+    public Grocery updateGrocery(@RequestBody Grocery grocery) {
+        return groceryService.updateGrocery(grocery);
     }
 }
--- Backend/src/main/java/com/HomeHero/demo/model/Grocery.java
+++ Backend/src/main/java/com/HomeHero/demo/model/Grocery.java
@@ -1,7 +1,5 @@
 package com.HomeHero.demo.model;
 
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -11,47 +9,25 @@ public class Grocery {
     private String grocery_name;
     private float grocery_cost;
     private OffsetDateTime created_at;
+    private UUID household_id;
 
     public Grocery(){}
 
-    public UUID getId() {
-        return id;
-    }
+    public UUID getId() { return id; }
+    public void setId(UUID id) { this.id = id; }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+    public UUID getProfile_id() { return profile_id; }
+    public void setProfile_id(UUID profile_id) { this.profile_id = profile_id; }
 
-    public UUID getProfileId() {
-        return profile_id;
-    }
+    public String getGrocery_name() { return grocery_name; }
+    public void setGrocery_name(String grocery_name) { this.grocery_name = grocery_name; }
 
-    public void setProfileId(UUID profile_id) {
-        this.profile_id = profile_id;
-    }
+    public float getGrocery_cost() { return grocery_cost; }
+    public void setGrocery_cost(float grocery_cost) { this.grocery_cost = grocery_cost; }
 
-    public String getGroceryName() {
-        return grocery_name;
-    }
+    public OffsetDateTime getCreated_at() { return created_at; }
+    public void setCreated_at(OffsetDateTime created_at) { this.created_at = created_at; }
 
-    public void setGroceryName(String grocery_name) {
-        this.grocery_name = grocery_name;
-    }
-
-    public float getGroceryCost() {
-        return grocery_cost;
-    }
-
-    public void setGroceryCost(float grocery_cost) {
-        this.grocery_cost = grocery_cost;
-    }
-
-    public OffsetDateTime getCreatedAt() {
-        return created_at;
-    }
-
-    public void setCreated_at(OffsetDateTime created_at) {
-        this.created_at = created_at;
-    }
-
+    public UUID getHousehold_id() { return household_id; }
+    public void setHousehold_id(UUID household_id) { this.household_id = household_id; }
 }
